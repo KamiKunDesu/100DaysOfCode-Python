a3ea82ecfe1ea0b4002b643233ccc24a08139f4e
KamiKunDesu <ikigaibouken@gmail.com>
2021-10-29 02:05:57 +0100

    Completes day 8 of the coding challenge

--- Day 8 - Caeser Cipher/caeser_cipher.py
+++ Day 8 - Caeser Cipher/caeser_cipher.py
@@ -13,26 +13,33 @@ letterGoodness = [0.0817, 0.0149, 0.0278, 0.0425, 0.127, 0.0223, 0.0202,
 def Encoder(string, S):
     Encoded_String = ""
     for s in string:
+        #store the ascii representation of the lowercase version of the letter
         char = ord(s.lower())
+        #basically if the ascii character wasn't alpha then skip
         if char not in range(97, 123):
             char = char
+        #If it is alpha do the shit
         else:
             for i in range(0, S):
+                #if get to top of the character list then restart (from z back to a basically)
                 if char == 122:
                     char = 96
                 char += 1
+        #turn it back into a character and append it to the string
         char = chr(char)
         Encoded_String += char
     return Encoded_String
 
-
+#this function can decode the message, the logic is the exact same as the encoder function but in reverse
 def Decoder(string, S):
     Decoded_String = ""
+    #logic below exactly the same as the encoder function
     for s in string:
         char = ord(s.lower())
         if char not in range(97, 123):
             char = char
         else:
+            #this part is different because instead of approaching top of character list we bottom out and go from a back to z
             for i in range(0, S):
                 if char == 97:
                     char = 123
@@ -41,34 +48,45 @@ def Decoder(string, S):
         Decoded_String += char
     return Decoded_String
 
-
+#This function checks the total goodness score of a given decode attempt
 def Total_Goodness_Check(string):
+    #Keep a track of the total goodness score of the string
     Total_Goodness = 0
+    #check each letter in the string
     for letter in string:
-        char = ord(letter)
-        if char == 32:
+        char = ord(letter.lower())
+        #if its not alpha, then skip over it
+        if char not in range(97, 123):
             char = char
+        #else if it is alpha then check it's position in the goodness list and add the total goodness score of
+        #that letter to the overall goodness score
         else:
-            char -= 65
+            #because the representation of a starts at 97, bring it back to 0 so that a is index 0 in the goodness list
+            char -= 97
             goodness_value = letterGoodness[char]
             Total_Goodness += goodness_value
     return Total_Goodness
 
-
+#This function checks the goodness score for the message decoded in all possible shifts and stores them in an array
 def All_Shift_Check(string):
+    #The array to store the goodness scores for each of the different shifts
     Goodness_Test = []
-    inputvar = string
+    #for each possible shift
     for i in range(0, 27):
-        Decoded_Message = Decoder(inputvar, i)
+        #First make the decoded message with the current shift
+        Decoded_Message = Decoder(string, i)
+        #Work out the goodness score for this decoding
         Message_Goodness = Total_Goodness_Check(Decoded_Message)
+        #Throw it in an array
         Goodness_Test.append(Message_Goodness)
     return Goodness_Test
 
-
+#This function works out the best shift to decode a message that has been shift encoded
 def Best_Choice(string):
+    #Kind of self explanatory but store a list of goodness scores for each possible shift
+    #Then store the index of the best score in shift (as this will be the shift)
+    #Then decode the message using the best shift, and return that
     Goodness_Test = All_Shift_Check(string)
     Shift = Goodness_Test.index(max(Goodness_Test))
     Best_Choice = Decoder(string, Shift)
     return Best_Choice
-
-print(Encoder("XyZ", 1))
